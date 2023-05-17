c8f4a6d7976611cf02c708f7708377c70972fd2f
Thắng <100744841+winmds@users.noreply.github.com>
2023-05-17 16:17:33 +0700

    update textinput

--- src/components/TextInputMoney/index.tsx
+++ src/components/TextInputMoney/index.tsx
@@ -3,46 +3,43 @@ import { TextInput } from 'react-native';
 
 export const TextInputMoney = ({ value, ...res }) => {
 
-    function formatMoney(number) {
+    function formatMoney(number: any): string {
         const numero = String(number)
             .replace(/\D/g, "")
             .replace(/^0+/, "");
 
         let counter = 1;
-        let isCommaFound = "notFound"
+        let isCommaFound = false;
 
-        const formattedStringReversed = Array
-            .from(numero.toString())
+        const formattedStringReversed = Array.from(numero.toString())
             .reverse()
             .reduce((acc, i, index, arr) => {
-                if (counter == 2 &&
-                    isCommaFound == "notFound" &&
-                    (arr.length > 2)) {
-                    acc = acc + i + ',';
-                    isCommaFound = "found";
+                if (counter === 3 && !isCommaFound && arr.length > 3) {
+                    acc = acc + i + ".";
+                    isCommaFound = true;
                     counter = 0;
-                } else if (counter === 3 && (arr.length != (index + 1))) {
-                    acc = acc + i + ','
+                } else if (counter === 3 && arr.length !== index + 1) {
+                    acc = acc + i + ".";
                     counter = 0;
                 } else {
-                    acc = acc + i
+                    acc = acc + i;
                 }
 
-                counter++
-                return acc
+                counter++;
+                return acc;
+            }, "");
 
-            }, "")
+        let formattedNumber = Array.from(formattedStringReversed)
+            .reverse()
+            .join("");
 
-        let formmatedNumber = Array.from(formattedStringReversed)
-            .reverse().join("")
-
-        if (formmatedNumber.length == 2) {
-            formmatedNumber = "0," + formmatedNumber
-        } else if (formmatedNumber.length == 1) {
-            formmatedNumber = "0,0" + formmatedNumber
+        if (formattedNumber.length === 2) {
+            formattedNumber = "0." + formattedNumber;
+        } else if (formattedNumber.length === 1) {
+            formattedNumber = "0.0" + formattedNumber;
         }
 
-        return formmatedNumber
+        return formattedNumber;
     }
 
     return (
