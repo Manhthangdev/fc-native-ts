afb286537db45bb7afa841c103f0e04c9ee86d30
Thắng <100744841+winmds@users.noreply.github.com>
2023-05-17 18:02:38 +0700

    update x

--- src/function/index.tsx
+++ src/function/index.tsx
@@ -254,6 +254,45 @@ const formatDate = (date: any) => {
   return dArr[2] + '/' + dArr[1] + '/' + dArr[0]; //ex out: "18/01/10"
 };
 
+function formatMoney(number: any): string {
+  const numero = String(number)
+    .replace(/\D/g, "")
+    .replace(/^0+/, "");
+
+  let counter = 1;
+  let isCommaFound = false;
+
+  const formattedStringReversed = Array.from(numero.toString())
+    .reverse()
+    .reduce((acc, i, index, arr) => {
+      if (counter === 3 && !isCommaFound && arr.length > 3) {
+        acc = acc + i + ".";
+        isCommaFound = true;
+        counter = 0;
+      } else if (counter === 3 && arr.length !== index + 1) {
+        acc = acc + i + ".";
+        counter = 0;
+      } else {
+        acc = acc + i;
+      }
+
+      counter++;
+      return acc;
+    }, "");
+
+  let formattedNumber = Array.from(formattedStringReversed)
+    .reverse()
+    .join("");
+
+  if (formattedNumber.length === 2) {
+    formattedNumber = "0." + formattedNumber;
+  } else if (formattedNumber.length === 1) {
+    formattedNumber = "0.0" + formattedNumber;
+  }
+
+  return formattedNumber;
+}
+
 export {
   formatDate,
   avatarOptions,
@@ -277,7 +316,8 @@ export {
   getPercent,
   handleRemoveSpace,
   checkUserNameVNese,
-  checkImages
+  checkImages,
+  formatMoney
 };
 
 
